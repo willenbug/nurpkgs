e7e2764574763cfd7615fe9fd7f4d6cb48cb10de
willenbug <76489193+willenbug@users.noreply.github.com>
2025-09-02 14:44:04 -0600

    Update default.nix

--- pkgs/atlassian-cli/default.nix
+++ pkgs/atlassian-cli/default.nix
@@ -1,8 +1,8 @@
 { stdenv, fetchurl, lib }:
 
 stdenv.mkDerivation rec {
-  pname = "atlassiancli";
-  version = "latest";
+  pname = "atlassian-cli";
+  version = "1.3.3-stable";
 
   src = fetchurl {
     url = if stdenv.hostPlatform.system == "x86_64-linux"
@@ -11,9 +11,9 @@ stdenv.mkDerivation rec {
       then "https://acli.atlassian.com/linux/latest/acli_linux_arm64/acli"
       else throw "Unsupported platform: ${stdenv.hostPlatform.system}";
     hash = if stdenv.hostPlatform.system == "x86_64-linux"
-      then "068fjcgq1n2s86sq2nlx15wy7543zprmi2zpc119fvphkm4xarcx"
+      then "sha256-068fjcgq1n2s86sq2nlx15wy7543zprmi2zpc119fvphkm4xarcx"
       else if stdenv.hostPlatform.system == "aarch64-linux"
-      then "1kxgi468yfdryzyj5mhngf7gv221i6324jxxqbrcfr68s2gcv4w2"
+      then "sha256-1kxgi468yfdryzyj5mhngf7gv221i6324jxxqbrcfr68s2gcv4w2"
       else "";
   };
 
@@ -29,7 +29,8 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "Atlassian Command Line Interface (CLI)";
     homepage = "https://developer.atlassian.com/cloud/acli/";
-    license = licenses.unfree;
+    license = licenses.unfreeRedistributable;
+    sourceProvenance = [ sourceTypes.binaryNativeCode ];
     platforms = [ "x86_64-linux" "aarch64-linux" ];
     maintainers = with maintainers; [ "willenbug" ];
     mainProgram = "acli";
